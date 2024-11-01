16b9641858a4fbdb8b24cc12b6913ef0b9a6f968
SvetlanaGF <svgf@mail.ru>
2024-11-01 18:55:42 +0300

    мелкие правки

--- services/services.js
+++ services/services.js
@@ -11,3 +11,11 @@ burgerMenu.addEventListener('click', () => {
  // Переключаем класс active для отображения/скрытия меню
     menu.classList.toggle('active');
 });
+
+// Добавляем обработчик события для кликов по документу
+document.addEventListener('click', (event) => {
+    // Проверяем, был ли клик вне бургер-меню и самого меню
+    if (!burgerIcon.contains(event.target) && !menu.contains(event.target)) {
+        menu.classList.remove('active'); // Закрываем меню
+    }
+});
