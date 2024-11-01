f6aadf8bee5f4794e5585cc4c75537436b391638
SvetlanaGF <svgf@mail.ru>
2024-11-01 17:56:31 +0300

    изменила js

--- about/about.js
+++ about/about.js
@@ -1,7 +1,13 @@
+// Бургер-меню
+
 console.log('Скрипт загружен');
 
+// Находим элементы бургер-меню и меню
 const burgerMenu = document.querySelector('.burger-menu');
 const menu = document.querySelector('.menu');
+
+// Добавляем обработчик события клика на бургер-меню
 burgerMenu.addEventListener('click', () => {
+ // Переключаем класс active для отображения/скрытия меню
     menu.classList.toggle('active');
 });
--- contacts/contact.js
+++ contacts/contact.js
@@ -1,11 +1,16 @@
+// Бургер-меню
+
 console.log('Скрипт загружен');
 
+// Находим элементы бургер-меню и меню
 const burgerMenu = document.querySelector('.burger-menu');
 const menu = document.querySelector('.menu');
+
+// Добавляем обработчик события клика на бургер-меню
 burgerMenu.addEventListener('click', () => {
+ // Переключаем класс active для отображения/скрытия меню
     menu.classList.toggle('active');
 });
-
 
 document.getElementById('contactForm').addEventListener('submit', function (e) {
     e.preventDefault();
